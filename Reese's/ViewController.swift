7c2654bbb7c4b4d7fb92e1112584ff3c83ea776f
Bob Witmer <bwitmer@golden.net>
2022-05-24 17:20:57 -0400

    Section 2.8 Exercise 2

--- Reese's/ViewController.swift
+++ Reese's/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    let message1 = "You Got Peanut Butter in my Chocolate!"
+    let message2 = "You Got Chocolate in my Peanut Butter!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel.text = ""
+        messageLabel.backgroundColor = .yellow
+        messageLabel.numberOfLines = 4
     }
 
-
+    @IBAction func buttonHey(_ sender: Any) {
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
+            messageLabel.textAlignment = .right
+            messageLabel.textColor = .orange
+        }
+        else {
+            messageLabel.text = message1
+            messageLabel.textAlignment = .left
+            messageLabel.textColor = .brown
+        }
+    }
+    
 }
 
